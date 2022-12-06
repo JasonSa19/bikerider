c7c7469606999eb742c80421c6868e512bb8fdd8
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2022-12-06 21:01:18 +0100

    remove slide-in

--- src/components/globals/hamburger.js
+++ src/components/globals/hamburger.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { useContext } from "react";
 
 const Hamburger = () => {
-  const MenuContext = (() => {
-    if (typeof window !== "undefined") {
-      return require("react-flexible-sliding-menu");
-    }
-  })();
-  const { toggleMenu } = useContext(MenuContext);
   return (
-    <button onClick={toggleMenu} className="hamburger-toggler">
+    <button className="hamburger-toggler">
       <div className="hamburger-toggler-line"></div>
       <div className="hamburger-toggler-line"></div>
       <div className="hamburger-toggler-line"></div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,25 +6,16 @@ import Header from "../components/globals/header";
 import Hero from "../components/globals/hero";
 import Footer from "../components/globals/footer";
 
-import Menu from "../components/globals/menu";
-
 // Import Menu Components
 
 // Init Components
 const IndexPage = () => {
-  const MenuProvider = (() => {
-    if (typeof window !== "undefined") {
-      return require("react-flexible-sliding-menu");
-    }
-  })();
   return (
-    <MenuProvider width="100%" MenuComponent={Menu}>
-      <>
-        <Header />
-        <Hero />
-        <Footer />
-      </>
-    </MenuProvider>
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
   );
 };
 
