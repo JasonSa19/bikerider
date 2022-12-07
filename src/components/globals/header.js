95556676c18ed273fbd2661fcbeb5740d85d40e0
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2022-12-07 09:16:32 +0100

    Change styles for header to fit menu

--- src/components/globals/header.js
+++ src/components/globals/header.js
@@ -11,27 +11,29 @@ const Header = () => {
   return (
     <header>
       <Menu />
-      <div className="header-wrap">
-        <div className="menu-toggler">
-          <Hamburger />
-        </div>
-        <div className="logo-wrap">
-          <AnchorLink to="/">
-            <StaticImage
-              className="login"
-              src="../../images/icon/person.svg"
-              alt="Person"
-            />
-          </AnchorLink>
-          <div className="seperator"></div>
+      <div className="header-outer-wrap">
+        <div className="header-wrap">
+          <div className="menu-toggler">
+            <Hamburger />
+          </div>
+          <div className="logo-wrap">
+            <AnchorLink to="/">
+              <StaticImage
+                className="login"
+                src="../../images/icon/person.svg"
+                alt="Person"
+              />
+            </AnchorLink>
+            <div className="seperator"></div>
 
-          <AnchorLink to="/">
-            <StaticImage
-              className="logo"
-              src="../../images/layout/logo-bikerider.svg"
-              alt="BikeRider Live Logo"
-            />
-          </AnchorLink>
+            <AnchorLink to="/">
+              <StaticImage
+                className="logo"
+                src="../../images/layout/logo-bikerider.svg"
+                alt="BikeRider Live Logo"
+              />
+            </AnchorLink>
+          </div>
         </div>
       </div>
     </header>
--- src/components/globals/menu.js
+++ src/components/globals/menu.js
@@ -1,20 +1,21 @@
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 import React from "react";
 
 const Menu = () => {
   return (
     <nav>
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/salads">
-        Salads
-      </a>
-      <a className="menu-item" href="/pizzas">
-        Pizzas
-      </a>
-      <a className="menu-item" href="/desserts">
-        Desserts
-      </a>
+      <AnchorLink className="menu-item" to="#leistungen">
+        Leistungen
+      </AnchorLink>
+      <AnchorLink className="menu-item" to="/treffen">
+        Treffen
+      </AnchorLink>
+      <AnchorLink className="menu-item" to="/touren">
+        Touren
+      </AnchorLink>
+      <AnchorLink className="menu-item" to="/trainings">
+        Trainings
+      </AnchorLink>
     </nav>
   );
 };
