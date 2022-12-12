df0f3992cf8839752ebd55c4964a47bce185fff6
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2022-12-12 23:15:59 +0100

    imprint page

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -6,18 +6,67 @@ import Footer from "../components/globals/footer";
 import HeroSub from "../components/hero-subpage/herosub";
 
 // Needed Resources
-import image from "../images/layout/trainings.jpg";
+import image from "../images/layout/cotedazur.jpg";
+
+import logo from "../images/layout/logo-bikerider.svg";
 
 const Impressum = () => {
   return (
     <>
       <Header />
       <HeroSub
-        imgClass="object-top"
+        imgClass="object-center"
         heroSubImage={image}
-        heroSubImageAlt="Trainings"
-        heroSubHeadline="Trainings"
+        heroSubImageAlt="Impressum"
+        heroSubHeadline="Impressum"
       />
+      <section className="info-section-imprint">
+        <div className="info">
+          <div className="info-text">
+            <span className="info-intro">
+              <strong>Verantwortlich für den Inhalt:</strong>
+              <br />
+              BikeRider Live GmbH, Krümmerberg
+            </span>
+            <div className="contact-information">
+              <span>
+                <strong>Telefon:</strong>
+                <br />
+                (0 12 34) 56 78
+                <br />
+                <br />
+                <strong>Fax:</strong>
+                <br />
+                (0 12 34) 56 79
+              </span>
+              <span>
+                <strong>E-Mail:</strong>
+                <br />
+                info@big-bang-net.de
+                <br />
+                <br />
+                <strong>Internet:</strong>
+                <br />
+                big-bang-net-consult.de
+              </span>
+              <span>
+                <strong>Technische Umsetzung der Internetseiten </strong>
+                <br />
+                Big Bang Net Consult GmbH
+                <br />
+                Motocross-Straße 7
+                <br />
+                35781 Ofenhausen
+              </span>
+            </div>
+          </div>
+          <div className="info-logo">
+            <div className="info-logo-inner">
+              <img src={logo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
